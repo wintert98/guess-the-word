a4ba775ebcb832d347ca9d52d4f2462cbe0f2d6d
wintert98 <38222798+wintert98@users.noreply.github.com>
2021-11-29 10:59:18 -0500

    Updated js to include checkInput and makeGuess functions

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ const remainSpan = document.querySelector(".remain");
 const message = document.querySelector(".message");
 const btnPlayAgain = document.querySelector(".play-again");
 const word = "magnolia";
+const guessedLetters = [];
 
 const updateText = function (word) {
     const circle = "●";
@@ -23,4 +24,29 @@ updateText(word);
 btnGuess.addEventListener("click", function (e) {
     e.preventDefault();
     console.log(input.value);
+    message.innerText = "";
+    const checkedInput = checkInput(input.value);
+    makeGuess(checkedInput);
 });
+
+const checkInput = function (input) {
+   const acceptedLetter = /[a-zA-Z]/
+   if (!input.match(acceptedLetter)) {
+       message.innerText = "Please enter only letters from a to z, and not a blank space."
+   } else if (input.length > 1 ) {
+    message.innerText = "Please enter only one letter"
+   } else {
+       return input
+   } 
+};
+
+const makeGuess = function (letter) {
+ const guess = letter.toUpperCase();
+ if (guessedLetters.includes(guess)) {
+     message.innerText = "Try again, you have already guessed that letter";
+ } else {
+     guessedLetters.push(guess);
+     console.log(guessedLetters);
+ }
+};
+
