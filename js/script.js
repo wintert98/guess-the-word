963918adae9f8077f86685ebf41bc78e6d90417f
wintert98 <38222798+wintert98@users.noreply.github.com>
2021-11-29 13:07:38 -0500

    Update js with functions to show the guessed letters, update the word in progress and check if the player has won

--- js/script.js
+++ js/script.js
@@ -8,6 +8,9 @@ const message = document.querySelector(".message");
 const btnPlayAgain = document.querySelector(".play-again");
 const word = "magnolia";
 const guessedLetters = [];
+guessed.innerHTML = "";
+
+/* -------- Updates Text of word to guess with circles -------------*/
 
 const updateText = function (word) {
     const circle = "●";
@@ -21,6 +24,8 @@ const updateText = function (word) {
 
 updateText(word);
 
+/* -------- Listens to button clicks and checks letters entered -------------*/
+
 btnGuess.addEventListener("click", function (e) {
     e.preventDefault();
     console.log(input.value);
@@ -28,6 +33,8 @@ btnGuess.addEventListener("click", function (e) {
     const checkedInput = checkInput(input.value);
     makeGuess(checkedInput);
 });
+
+/* -------- Updates Text of word to guess with circles -------------*/
 
 const checkInput = function (input) {
    const acceptedLetter = /[a-zA-Z]/
@@ -40,13 +47,53 @@ const checkInput = function (input) {
    } 
 };
 
+/* -------- Checks all letters guessed -------------*/
 const makeGuess = function (letter) {
  const guess = letter.toUpperCase();
  if (guessedLetters.includes(guess)) {
      message.innerText = "Try again, you have already guessed that letter";
  } else {
      guessedLetters.push(guess);
-     console.log(guessedLetters);
+     showGuess(guess);
+     updateWordInProgress(guessedLetters);
  }
 };
 
+/* -------- Shows all letters guessed to DOM -------------*/
+const showGuess = function (guess) {
+  //guessed.innerHTML = "";
+  let li = document.createElement("li");
+  li.innerHTML = `<li> ${guess} </li>`
+  guessed.append(li);
+};
+
+/* -------- Updates Text of word in progress to guess and removes circles of correctly guessed letters-------------*/
+
+const updateWordInProgress = function (array) {
+  const wordUpper = word.toUpperCase();
+  const wordArray = wordUpper.split("");
+  let newWordArray = [];
+  for (let letter of wordArray) {
+      console.log(letter);
+      if (array.includes(letter)) {
+          newWordArray.push(letter);
+      } else {
+          newWordArray.push("●")
+      }
+  }
+  let joined = newWordArray.join("")
+  wordInProgess.innerText = joined;
+  console.log(joined);
+  playerWin(joined)
+};
+
+/* -------- Checks if player has won the game -------------*/
+const playerWin = function (wordGuess) {
+    const wordUpCase = word.toUpperCase();
+    if (wordUpCase === wordGuess) {
+        message.classList.add("win")
+        message.innerHTML =  `<p class="highlight">You guessed correct the word! Congrats!</p>`
+
+    } 
+
+};
